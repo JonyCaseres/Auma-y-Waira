1f0b910f45dfb5927971c9edf8854479b2ed2791
JonyCaseres <jonitco130@gmail.com>
2026-06-01 12:41:13 -0400

    CO

--- Assets/Screpts/Player.cs
+++ Assets/Screpts/Player.cs
@@ -193,17 +193,34 @@ public class Player : MonoBehaviour, InputSystem_Actions.IPlayerActions
 
         foreach (Collider2D collider in colliders)
         {
-            IInteractable interactable = collider.GetComponent<IInteractable>();
-            if (interactable == null)
+            // Ignorar el propio collider del jugador para no detectar al Player como un interactable.
+            if (collider.transform.IsChildOf(transform))
             {
-                interactable = collider.GetComponentInParent<IInteractable>();
+                Debug.Log($"Skipping own player collider {collider.name}");
+                continue;
             }
 
-            if (interactable != null && interactable.CanInteract())
+            IInteractable interactable = collider.GetComponent<IInteractable>()
+                ?? collider.GetComponentInParent<IInteractable>()
+                ?? collider.GetComponentInChildren<IInteractable>();
+
+            if (interactable != null)
             {
-                Debug.Log($"Interacting with {collider.name} ({interactable.GetType().Name})");
-                interactable.Interact(gameObject);
-                return;
+                Debug.Log($"Found interactable on {collider.name} => {interactable.GetType().Name}");
+                if (interactable.CanInteract())
+                {
+                    Debug.Log($"Interacting with {collider.name} ({interactable.GetType().Name})");
+                    interactable.Interact(gameObject);
+                    return;
+                }
+                else
+                {
+                    Debug.Log($"Interactable found but not ready: {collider.name}");
+                }
+            }
+            else
+            {
+                Debug.Log($"No IInteractable on collider {collider.name}. Checking next.");
             }
         }
         Debug.Log("InteractNearby: no interactable found or none were ready.");
